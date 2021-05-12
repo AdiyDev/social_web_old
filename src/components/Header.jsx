06a7454da8f1acadecdf493251aeacee2a3853e4
AdiyDev <kain011@ya.ru>
2021-05-12 11:13:22 +0300

    added project on github

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className="header">
+        <img src="https://cdn.logo.com/hotlink-ok/logo-social.png" alt=""></img>
+    </header>
+}
+
+export default Header;
