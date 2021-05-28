3427eedf6747769d65b1f865ff365ddcae173d7a
AdiyDev <kain011@ya.ru>
2021-05-28 18:04:25 +0300

    added css style in navbar

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,20 @@ import s from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <ul className={s.dialogsItems}>
+                <li className={s.dialog + ' ' + s.active}>Arkady</li>
+                <li className={s.dialog}>Ludmila</li>
+                <li className={s.dialog}>Slavik</li>
+                <li className={s.dialog}>Danis</li>
+                <li className={s.dialog}>Elena</li>
+                <li className={s.dialog}>Pasha</li>
+            </ul>
+            <ul className={s.messages}>
+                <li className={s.message}>Hi</li>
+                <li className={s.message}>How are your samuraiJS</li>
+                <li className={s.message}>privet</li>
+            </ul>
         </div>
     )
 }
