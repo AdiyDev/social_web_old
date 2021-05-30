4b8318546a18f150caa95c77a96361eb04ae08ca
AdiyDev <kain011@ya.ru>
2021-05-30 18:09:02 +0300

    change in Route component= on render={()=>{}}

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import {BrowserRouter, Route} from "react-router-dom";
 
+// let ProfileComponent = () => <Profile/>;
+
 const App = () => {
     return (
         <BrowserRouter>
@@ -16,11 +18,13 @@ const App = () => {
                 <Header/>
                 <Navbar/>
                 <div className="app-wrapper-content">
-                    <Route path='/profile' component={Profile}/>
-                    <Route path='/dialogs' component={Dialogs}/>
-                    <Route path='/news' component={News}/>
-                    <Route path='/music' component={Music}/>
-                    <Route path='/settings' component={Settings}/>
+                    <Route path='/profile' render={()=> <Profile/>}/>
+                    <Route path='/dialogs' render={()=> <Dialogs/>}/>
+                    <Route path='/news' render={()=> <News/>}/>
+                    <Route path='/music' render={()=> <Music/>}/>
+                    <Route path='/settings' render={()=> <Settings/>}/>
+                    {/*<Route path='/profile' component={ProfileComponent}/> так же можно создать выже через let функцию дать ей название и передать название внутрь component и props кинуть выше*/}
+                    {/*route path='' render={() => {<Profile/>}} анонимная функция*/}
                 </div>
             </div>
         </BrowserRouter>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,27 +5,18 @@ import Post from "./Post/Post";
 
 const MyPosts = (props) => {
 
-    let posts = [
-        {id: 1, message: "Hi, how are you? ", likesCount: " 15"},
-        {id: 2, message: "It's my first post ", likesCount: " 10"},
-        {id: 3, message: "privet ", likesCount: " 7"},
-        {id: 4, message: "Zdarov ", likesCount: " 3"},
-        {id: 5, message: "Kak dela? ", likesCount: " 2"},
-        {id: 6, message: "Vse normalno ", likesCount: " 1"}
-    ]
-
-    let postsElements = posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>);
+    let postsElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>);
 
     return (
         <ul className={s.postsBlock}>
-            <li className={s.myPostsBlock}>
-                <h3>My posts</h3>
-                <textarea></textarea>
-                <button className={s.buttonBlue + ' ' + s.buttonBlueEffect + ' ' + s.buttonBlueRotate}>add post</button>
-            </li>
-            <li className={s.posts}>
+            <ul className={s.myPostsBlock}>
+                <li><h3>My posts</h3></li>
+                <li><textarea></textarea></li>
+                <li><button className={s.buttonBlue + ' ' + s.buttonBlueEffect + ' ' + s.buttonBlueRotate}>add post</button></li>
+            </ul>
+            <ul className={s.posts}>
                 {postsElements}
-            </li>
+            </ul>
         </ul>
     )
 }
