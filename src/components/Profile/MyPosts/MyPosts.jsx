8e112e7402d19237b72a378b6d9ea1b7bbe4f67d
adiyDev <adiy.personal.mail@ya.ru>
2021-06-24 12:17:59 +0300

    new fix

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import s from "./MyPosts.module.css";
 import Post from './Post/Post';
 import { reduxForm, Field } from 'redux-form';
+import { required, maxLengthCreator } from './../../../utils/validators/validators';
 
+
+const maxLength10 = maxLengthCreator(10)
 
 const MyPosts = (props) => {
   let postsElements = props.profilePage.posts.map(p => <Post message={p.message} key={p.id} likesCount={p.likesCount} />);
@@ -26,10 +29,10 @@ const MyPosts = (props) => {
 const AddNewPostForm = (props) => {
   return <form onSubmit={props.handleSubmit} >
     <h3>My posts</h3>
-    <Field name='newPostText' component='textarea' />
+    <Field name='newPostText' component='textarea'
+      validata={[required, maxLength10]} />
     <button className={s.buttonBlue + ' ' + s.buttonBlueEffect + ' ' + s.buttonBlueRotate}>add post</button>
   </form>
-
 }
 
 const AddNewPostFormRedux = reduxForm({ form: 'ProfileAddNewPostForm' })(AddNewPostForm)
