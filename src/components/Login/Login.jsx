d1f14ce75a9da7e4b4741cc16b129d0cd8f38602
adiyDev <adiy.personal.mail@ya.ru>
2021-06-24 14:14:18 +0300

    validators in login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
+import { Element } from './../common/FormsControls/FormsControls'
+import { required, maxLengthCreator } from './../../utils/validators/validators';
+
+const Input = Element("input")
+const maxLength30 = maxLengthCreator(30)
 
 const LoginForm = (props) => {
   return (
     <div>
       <form onSubmit={props.handleSubmit}>
         <div>
-          <Field placeholder={'Login'} name={'login'} component={'input'} />
+          <Field placeholder={'Login'} name={'login'} component={Input} validate={[required, maxLength30]} />
         </div>
         <div>
-          <Field placeholder={'Password'} name={'password'} component={'input'} />
+          <Field placeholder={'Password'} name={'password'} component={Input} validate={[required, maxLength30]} />
         </div>
         <div>
-          <Field type={'checkbox'} name={'rememberMe'} component={'input'} /> remebmer me
+          <Field type={'checkbox'} name={'rememberMe'} component={Input} validate={[required, maxLength30]} /> remebmer me
         </div>
         <div>
           <button>Login</button>
--- src/components/common/FormsControls/FormsControls.js
+++ src/components/common/FormsControls/FormsControls.js
@@ -3,31 +3,11 @@ import styles from './FormsControls.module.css'
 // rest оператор сделаем в пропс пропсы будут содержать всё кроме input и meta
 
 export const Element = Element => ({ input, meta, ...props }) => {
-  const hasError = meta.touched && meta.error;
+  const hasError = meta.touched && meta.error
   return (
     <div className={styles.formControl + " " + (hasError ? styles.error : "")}>
       <Element {...input} {...props} />
       {hasError && <span> {meta.error} </span>}
     </div>
-  );
-};
-
-
-// export const Textarea = Element("textarea");
-
-
-// export const Textarea = ({ input, meta, ...props }) => {
-
-//   const hasError = meta.touched && meta.error
-//   return (
-//     <div className={styles.formControl + ' ' + (hasError ? styles.error : '')}>
-//       {/* все пропсы приходящии в нас отдать конечному потребителю */}
-//       <div>
-//         <textarea {...input} {...props} />
-//       </div>
-//       <div>
-//         {hasError && <span> {meta.error}</span>}
-//       </div>
-//     </div >
-//   )
-// }
+  )
+}
