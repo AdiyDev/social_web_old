439616cf67a6209f9ba830651ed7a3b473995b04
Arkadiy B <adiy.work.mail@ya.ru>
2022-05-17 21:32:08 +0300

    chore: test ssh

--- src/components/common/Paginator/Paginator.test.jsx
+++ src/components/common/Paginator/Paginator.test.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { create } from "react-test-renderer";
 import Paginator from "./Paginator";
-
+//test
 describe("Paginator component tests", () => {
   test("pages count is 11 but should be showed only 10", () => {
     const component = create(<Paginator totalItemsCount={11} pageSize={1} portionSize={10} />);
